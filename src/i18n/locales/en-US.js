7e3d851772302f6541faa5f39de5bf859211e75c
yhy <2372135022@qq.com>
2026-04-09 18:43:16 +0800

    我添加了国际化和一键换肤功能

---
+++ src/i18n/locales/en-US.js
@@ -0,0 +1,18 @@
+export default {
+  login: {
+    title: 'User Login',
+    username: 'Please enter username',
+    password: 'Please enter password',
+    loginBtn: 'Login',
+    usernameRequired: 'Please enter username',
+    passwordRequired: 'Please enter password',
+    passwordInvalid: 'Password length cannot be less than 6 characters',
+    loginSuccess: 'Login successful',
+    loginFailed: 'Login failed, please try again later'
+  },
+  common: {
+    language: 'Language',
+    chinese: '中文',
+    english: 'English'
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'normalize.css'
 import './style.css'
+import i18n from './i18n'
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+const pinia = createPinia()
+createApp(App).use(router).use(ElementPlus).use(i18n).use(pinia).mount('#app')
